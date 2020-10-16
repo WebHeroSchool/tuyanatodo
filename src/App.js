8a6ca8f38d59a0c9c261ae9e627d97e98aa32348
Tuyana Tsydenova <tsidenovatb@gmail.com>
2020-10-17 01:09:02 +0400

    save

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+let number = 56;
+let a = 'ab';
+let b = 'cba';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p style={{
+        	color: 'green',
+        	fontSize: 32
+        }}>
+        Hello, its me
+        </p>
+        <p>
+        	{number}
+        </p>
+        <p>
+        	{4568}
+        </p>
+        <p>
+        	{89 + (456 * 2)}
+        </p>
+        <p>
+        	{a == 'ab' && 'ok'}
+
+        </p>
+        <p>
+        	{a == 'abc' ? 'ok' : 'not ok'}
+        </p>
+        <span>
+        	{undefined}
+        	{null}
+        	{false}
+        	{true}
+        </span>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
