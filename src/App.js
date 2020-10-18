d1de826f4cf2394ed84f99ed5f42b10b81213572
Tuyana Tsydenova <tsidenovatb@gmail.com>
2020-10-19 00:13:13 +0400

    save1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const element = (<div>
+const ItemList = () => (<ol>
+	<li>first</li>
+	<li>second</li>
+	<li>third</li>
+	</ol>);
+
+const App = () => (<div>
 	<h1>Hello!!!</h1>
 	<p>text text text</p>
 	<img src="" alt="pic"></img>
+	<ItemList />
 	</div>);
 
-export { element };
+export default App;
