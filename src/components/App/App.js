e5229c795c2832f732c34d11de6ad4a466301a10
Tuyana Tsydenova <tsidenovatb@gmail.com>
2020-10-21 23:01:19 +0400

    save

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,14 +3,14 @@ import ItemList from '../ItemList/ItemList';
 import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
 
-
+const mustDo = 'to do first';
 const App = () => (<div>
 	<h1>Hello!!!</h1>
 	<p>text text text</p>
 	<img src="" alt="pic"></img>
 	<InputItem />
-	<ItemList />
-	<Footer />
+	<ItemList mustDo={mustDo} />
+	<Footer number={10} />
 	</div>);
 
 export default App;
