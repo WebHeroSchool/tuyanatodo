6b36f7e7e71af53dae074726afe82ce085a479c1
Tuyana Tsydenova <tsidenovatb@gmail.com>
2020-10-27 23:52:14 +0400

    save2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import ItemList from '../ItemList/ItemList';
 import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
 import styles from './App.module.css';
+import Button from '@material-ui/core/Button';
 
 
 const App = () => {
@@ -26,11 +27,14 @@ const App = () => {
 	<div className={styles.main}>
 		<h1 className={styles.title}>Hello!!!</h1>
 		<p className={styles.text}>text text text</p>
-		<img src="" alt="pic"></img>
+		<p><img src="" alt="pic"></img></p>
 		<InputItem />
 		<ItemList items={items} />
 		<Footer number={10} />
-	</div>);
+		<Button variant="contained" color="primary" href="#contained-buttons">
+        Link
+      	</Button>
+    </div>);
 }
 
 export default App;
--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
-const InputItem = () => (<h3>List of Items</h3>);
+const InputItem = () => (<div>
+	<TextField
+        id="outlined-helperText"
+        label="Helper text"
+        defaultValue="Default Value"
+        helperText="Some important text"
+        variant="outlined"
+    />
+</div>);
 
 export default InputItem;
