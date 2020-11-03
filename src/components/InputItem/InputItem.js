bbec5187c2c9747d56a5cea9608791ae57466f3f
Tuyana Tsydenova <tsidenovatb@gmail.com>
2020-11-04 00:00:32 +0400

    save

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -2,11 +2,16 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import styles from './InputItem.module.css';
 
-const InputItem = () => (<div className={styles.input}>
-	<TextField className={styles.textfield}
-     id="standard-basic"
-     label="Add an item" 
-    />
-</div>);
+class InputItem extends React.Component {
+	render() {
+		return (
+		<div className={styles.input}>
+			<TextField className={styles.textfield}
+		     id="standard-basic"
+		     label="Add an item" 
+		    />
+		</div>);
+	}
+}
 
 export default InputItem;
